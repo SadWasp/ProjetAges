7bfff8c4d225a481f287b50d59745c48223f94b0
SadWasp <zacharie.lassey@hotmail.ca>
2021-11-04 22:02:50 -0400

    sprint2

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('items/<int:pk>/update/', views.updateItem),
     path('items/<int:pk>/delete/', views.deleteItem),
     path('items/<int:pk>/', views.getItem),
+    path('items/<int:pk>/', views.getItemWithScan),
 
     path('users/', views.getUsers),
     path('users/create/', views.createUser),
@@ -18,4 +19,25 @@ urlpatterns = [
     path('auth/login', views.authLogin),
     path('auth/<int:pk>/logout', views.authLogout),
     path('auth/register', views.authReg),
-]
+
+
+    path('orders/<int:pk>/', views.getAllOrdersFrom),
+    path('orders/', views.getAllOrders),
+
+
+    #whole order:
+    path('orders/<int:pk>/create/', views.createOrder),
+
+    path('orders/<int:pk>/delete/', views.deleteOrder),
+
+
+    #items in order:
+    path('orders/<int:orderId>/<int:itemId>/delete/', views.deleteItemInOrder),
+    path('orders/<int:orderId>/update/', views.updateOrder),
+
+    #path('orders/<int:orderId>/addItem/', views.addItemInOrder),
+    #path('orders/<int:orderId>/<int:itemId>/modify/', views.modifyItemInOrder),
+
+    path('locations/', views.getLocations),
+    path('locations/create/', views.createLocation),
+    path('locations/<int:pk>/getItems/', views.getItemsAtLocation),
